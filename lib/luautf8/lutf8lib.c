9b2a7d81caf1dbd9aae0df4bcfd44d0dcf755975
monk <admin@monk.moe>
2026-08-21 11:06:58 -0300

    Include limits.h for INT_MAX in luautf8

--- lib/luautf8/lutf8lib.c
+++ lib/luautf8/lutf8lib.c
@@ -7,6 +7,7 @@
 
 #include <assert.h>
 #include <string.h>
+#include <limits.h>
 
 #include "unidata.h"
 
@@ -1297,4 +1298,3 @@ LUALIB_API int luaopen_utf8 (lua_State *L) {
  * win32cc: run='lua.exe test.lua'
  * maccc: run='lua -- test_compat.lua'
  * maccc: flags+='-g --coverage -bundle -undefined dynamic_lookup' output='lua-utf8.so' */
-
